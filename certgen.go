1c877492c972b13243850457fef69b31c6a2cab9
George Miller <git@duffleman.co.uk>
2020-11-04 00:10:04 +0000

    release version 1!

--- certgen.go
+++ certgen.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Version of the CLI tool
-var Version = semver.MustParse("0.0.1").String()
+var Version = semver.MustParse("1.0.0").String()
 
 const (
 	// RootFolder is the folder without any additions
